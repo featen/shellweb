8556ab4048f82a0e247b44a7c50b2d98eca3c5d8
Jianjun <jianjun@x1.featen.com>
2019-01-12 00:24:36 -0800

    get the full web site done.

--- http.sh
+++ http.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-respond() {
+RespondHtml ()
+{
     echo "HTTP/1.1 200 OK"
     echo "Connection: close"
-    echo "Content-Type: $contentType; charset=utf-8"
+    echo "Content-Type: $content_type; charset=utf-8"
     echo "Content-Length: ${#body}"
     echo "Host: $host"
     echo "Date: $(TZ=UTC; date '+%a, %d %b %Y %T GMT')"
@@ -20,12 +21,14 @@ fi
 path="${path%%\?*}"
 
 case "$path" in
-    *.html) contentType='text/html' ;;
-    *.jpg|*.jpeg) contentType='image/jpeg' ;;
-    *.png) contentType='image/png' ;;
-    *.ico) contentType='image/x-icon' ;;
-    *.js) contentType='application/x-javascript' ;;
-    *) contentType='application/octet-stream';;
+    *.html) content_type='text/html' ;;
+    *.md) content_type='text/html'; type=markdown ;;
+    *.jpg|*.jpeg) content_type='image/jpeg'; type=bin ;;
+    *.png) content_type='image/png'; type=bin ;;
+    *.ico) content_type='image/x-icon'; type=bin ;;
+    *.js) content_type='application/x-javascript' ;;
+    *.css) content_type='text/css' ;;
+    *) content_type='application/octet-stream'; type=bin ;;
 esac
 
 while :
@@ -39,19 +42,37 @@ do
 done
 
 case "$host" in 
-    *) rootDir='/var/share/html/default' ;;
+    *) root_dir="/home/jianjun/code/shellweb/html" ;;
 esac
 
-filePath="${rootDir}/${path}"
-if [[ -f "$filePath" ]]
+file_path="${root_dir}/${path}"
+file_header="${root_dir}/header.html"
+file_footer="${root_dir}/footer.html"
+
+if [[ -f "$file_path" ]]
 then
-    body=$(<$filePath)
+    if [[ "$type" == "bin" ]]; then 
+        echo 'HTTP/1.1 200 OK'
+        echo -e "Content-Type: $content_type; charset=binary\n"
+        cat "$file_path"
+        exit 0
+    elif [[ "$type" == "markdown" ]]
+    then
+        mk_body=$(markdown $file_path)
+        page_header=$(<$file_header)
+        page_footer=$(<$file_footer)
+        body=${page_header}${mk_body}${page_footer}
+        RespondHtml
+    else
+        body=$(<$file_path)
+        RespondHtml
+        exit 0
+    fi
 else
     echo 'HTTP/1.1 404 Not Found'
     echo -e "\n404 Not Found"
     exit 0
 fi
 
-respond 
 exit 0
 
